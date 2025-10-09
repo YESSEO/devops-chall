6e5ee532fb8ad5c1da29f92ecbdae03adf303b56
YESSEO <thespacebirds@gmail.com>
2025-10-09 21:21:39 +0200

    tesT

--- wazuh-docker-4.13.1/build-docker-images/build-images.sh
+++ wazuh-docker-4.13.1/build-docker-images/build-images.sh
@@ -6,7 +6,6 @@ IMAGE_VERSION=${WAZUH_IMAGE_VERSION}
 
 # Wazuh package generator
 # Copyright (C) 2023, Wazuh Inc.
-#
 # This program is a free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public
 # License (version 2) as published by the FSF - Free Software
